0bdd3056fe38556a3d0fe1e8ace582a27883242d
RachelTwede <rtwede@byui.edu>
2023-07-08 21:28:08 -0600

    cat and dog fact apis for care page working

--- src/js/model.mjs
+++ src/js/model.mjs
@@ -8,15 +8,6 @@ export async function getAnimalListJSON() {
 export async function getAnimalFactFromAPI(url) {
   const response = await fetch(url);
   return response.json();
-
-  // return fetch("https://catfact.ninja/fact?max_length=140")
-  // .then((response) => {
-  //   if (!response.ok) {
-  //     throw new Error(`HTTP error: ${response.status}`);
-  //   }
-  //   return response.json();
-  // })
-  // .catch((error) => {
-  //   //code for displaying error message
-  // });
 }
+
+
